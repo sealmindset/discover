6ae7497e472764c770d7b9f6bc1d4d883e506beb
sealmindset <sealmindset@gmail.com>
2016-02-23 05:50:58 -0600

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
-
+#
+# Copyright 2016... nmap.sh authors
+#
+# nmap.sh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# nmap.sh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with nmap.sh.  If not, see <http:#www.gnu.org/licenses/>.
+#
+# Description:
+#
+# This tool was meant to show the attack surface of any given device susceptible to vulnerabilities.  
+#
+# Two directories are created - output and results - output will contain the raw data, and results will provide the report.
+# smb-check-vulns.nse seems to be missing in th the lastest incarnation of nmap 7.0.1 so a copy (source: offensive-security) is made.
+# Part of the checks is to take a snapshot of any websites running on the next work, so using an updated version of Trustwave's 
+# http-screenshot.nse patched by afxdub
+#
+# The main tool is nmap, but additional logic will be incorporated as needed.  This script is based on thepcn3rd handy work, with
+# a couple of changes.
+#
+# To use: See usage
+#
 # Defaults
 output=${PWD}/output
 results=${PWD}/results
